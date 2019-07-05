b9935515c1258f8f86f4b4853aab7fe05449a88d
Supporterino <lars@roth-kl.de>
2019-07-05 18:41:23 +0200

    Python Port complete

--- generator.py
+++ generator.py
@@ -1,11 +1,14 @@
 import csv
 import random
+from random import randint
 import string
-import time
+from time import time
 from faker import Faker
+from faker.providers import misc
 
-RECORD_COUNT = 100
+RECORD_COUNT = 100000000
 fake = Faker()
+fake.add_provider(misc)
 
 def genName(env):
     zoneChooser = {
@@ -13,24 +16,24 @@ def genName(env):
         'Abnahme': 'a',
         'Produktion': 'p'
     }
-    return 'm999' + ''.join(random.choice(string.ascii_lowercase) for j in range(5)) + randint(100, 999) + zoneChooser.get(env)
+    return 'm999' + ''.join(random.choice(string.ascii_lowercase) for j in range(5)) + str(randint(100, 999)) + zoneChooser.get(env)
 
 def genTestdata():
-    with open('./data.csv', 'w', newline='') as csvfile:
+    with open('./data.csv', 'w+') as csvfile:
         fieldnames = ['UUID', 'Servername', 'OS', 'Zone', 'Anwendung']
         osTypes = ['MacOS', 'Linux', 'Windows']
         zoneTypes = ['Entwicklung', 'Abnahme', 'Produktion']
         applications = ['MongoDB', 'OracleDB', 'PostgreSQL', 'Cassandra', 'Jira', 'Confluence', 'Notes', 'GitLab', 'WebSphere', 'Wekan', 'NodeJS', 'Apache', 'TFS', 'Proxy', 'Loadbalancer', 'MSSQL', 'SharePoint', 'Exchange']
-        zone = random.choice(zoneTypes)
-        os = random.choice(osTypes)
         writer = csv.DictWriter(csvfile, fieldnames)
 
         writer.writeheader()
         for i in range(RECORD_COUNT):
+            zone = random.choice(zoneTypes)
+            os = random.choice(osTypes)
             writer.writerow(
                 {
                     'UUID': fake.uuid4(),
-                    'Servername': genName(),
+                    'Servername': genName(zone),
                     'OS': os,
                     'Zone': zone,
                     'Anwendung': random.choice(applications)
@@ -38,6 +41,7 @@ def genTestdata():
             )
 
 if __name__ == '__main__':
+    print('Generating {:,} assets'.format(RECORD_COUNT))
     start = time()
     genTestdata()
     elapsed = time() - start
